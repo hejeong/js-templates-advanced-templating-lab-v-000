5588c788eba737b3eabde98ee09abbf905d7525a
hejeong <jonhong777@gmail.com>
2019-03-01 15:38:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  let formTemplate = document.getElementById('recipe-form-template').innerHTML;
+  let templateFunction = Handlebars.compile(formTemplate);
+  let html = templateFunction();
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
